bfb646cc3f757d628edd336f900474890aa48cb6
IT23230910 <IT23230910@my.sliit.lk>
2024-07-23 13:30:36 +0530

    Version 1.0.0

---
+++ Question1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class StringManipulation {
+
+    public static void main(String[] args) {
+       
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your first name: ");
+        String firstName = scanner.nextLine();
+
+        System.out.print("Enter your middle name: ");
+        String middleName = scanner.nextLine();
+
+        System.out.print("Enter your last name: ");
+        String lastName = scanner.nextLine();
+
+        String fullName = lastName + ", " + firstName + " " + middleName;
+
+        System.out.println("Full Name: " + fullName);
+
+        scanner.close();
+    }
+}
